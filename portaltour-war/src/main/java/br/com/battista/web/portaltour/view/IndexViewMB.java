bac67e87c25d060c4fff6949d902036c76e68bbf
Rafael Alessandro Batista <rabsouza@gmail.com>
2014-08-26 21:02:40 -0300

    Configuração do log4j e web.xml. Prototipo finalizado

--- portaltour-war/src/main/java/br/com/battista/web/portaltour/view/IndexViewMB.java
+++ portaltour-war/src/main/java/br/com/battista/web/portaltour/view/IndexViewMB.java
@@ -5,6 +5,9 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import br.com.battista.core.portaltour.builder.IndexViewDTOBuilder;
 import br.com.battista.core.portaltour.dto.IndexViewDTO;
 
@@ -17,10 +20,12 @@ import br.com.battista.core.portaltour.dto.IndexViewDTO;
 public class IndexViewMB implements Serializable {
 
 	private static final long serialVersionUID = -2164938363833312532L;
+	private static final Logger LOGGER = LogManager.getLogger(IndexViewMB.class.getName());
 
 	private IndexViewDTO dto = null;
 
 	public IndexViewMB() {
+		LOGGER.debug(String.format("Criando a view: %s.", "Index.xhtml"));
 		setDto(new IndexViewDTOBuilder().setUsuario("Rafael").build());
 	}
 
